ea8bf19a8ba52ba3adb6ff0ce071e9d5c317bc7f
Marc <recatala.marc@gmail.com>
2017-12-20 21:22:17 +0100

    Adapted login to new req.body format

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,8 +4,9 @@ const User = require("../models/User");
 
 router.post("/", (req, res) => {
 	console.log( req.body );
-	User.findOne({ username: req.body.username }).then( user => {
-		if ( user && user.isValidPassword( req.body.password ) ) {
+	const { credentials } = req.body;
+	User.findOne({ username: credentials.username }).then( user => {
+		if ( user && user.isValidPassword( credentials.password ) ) {
 			res.json({ user: user.toAuthJSON() });
 		} else {
 			res.status( 400 ).json({ errors: { global: "Invalid credentials" } });
